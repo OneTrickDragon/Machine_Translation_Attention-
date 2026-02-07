f7b0a432edcdfcd1c742bd754d305e1f1a5123f2
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-02-07 05:17:20 -0500

    Dataset

--- NMT.py
+++ NMT.py
@@ -168,7 +168,7 @@ class NMTVectorizer(object):
         return cls(source_vocab = source_vocab,
                    target_vocab = target_vocab,
                    max_source_length = contents["max_source_length"],
-                   max_target_length = contents["max_target_length"])
+                   max_target_length = contents["max]_target_length"])
     
     def to_serializable(self):
         return{"source vocab": self.source_vocab.to_serializable(),
@@ -177,3 +177,66 @@ class NMTVectorizer(object):
                "max_target_length": self.max_target_length}
 
 
+class NMTDataset(Dataset):
+    def __init__(self, text_df, vectorizer):
+        self._text_df = text_df
+        self._vectorizer = vectorizer
+
+        self.train_df = self._text_df[self._text_df.split=='train']
+        self.train_size = len(self.train_df)
+
+        self.val_df = self._text_df[self._text_df.split=='val']
+        self.val_size = len(self.val_df)
+
+        self.test_df = self._text_df[self._text_df.split=='test']
+        self.test_size = len(self.test_df)
+
+        self._lookup_dict = {'train': (self.train_df, self.train_size),
+                             'val': (self.val_df, self.val_size),
+                             'test': (self.test_df, self.test_size)}
+        
+        self.set_split("train")
+
+    @classmethod
+    def load_dataset_and_make_vectorizer(cls, dataset_csv):
+        text_df = pd.read_csv(dataset_csv)
+        train_subset = text_df[text_df.split=='train']
+        return cls(text_df, NMTVectorizer.from_dataframe(train_subset))
+
+    @classmethod 
+    def load_datatset_and_load_vectorizer(cls, dataset_csv, vectorizer_filepath):
+        text_df = pd.read_csv(dataset_csv)
+        vectorizer = cls.load_vectorizer_only(vectorizer_filepath)
+        return cls(text_df, vectorizer)
+    
+    @staticmethod
+    def load_vectorizer_only(vectorizer_filepath):
+        with open(vectorizer_filepath) as fp:
+            return NMTVectorizer.from_serializable(json.load(fp))
+        
+    def save_vectorizer(self, vectorizer_filepath):
+        with open(vectorizer_filepath, "w") as fp:
+            json.dump(self._vectorizer.to_serializable(), fp)
+
+    def get_vectorizer(self):
+        return self._vectorizer
+
+    def set_split(self, split="train"):
+        self._target_split = split
+        self._target_df, self._target_size = self._lookup_dict[split]
+
+    def __len__(self):
+        return self._target_size
+
+    def __getitem__(self, index):
+        row = self._target_df.iloc[index]
+
+        vector_dict = self._vectorizer.vectorize(row.source_language, row.target_language) 
+
+        return {'x_source': vector_dict['source_vector'],
+                'x_target': vector_dict['target_x_vector'],
+                'y_target': vector_dict['target_y_vector'],
+                'x_source_length': vector_dict['source_length']}
+    
+    def get_num_batches(self, batch_size):
+        return len(self)//batch_size
